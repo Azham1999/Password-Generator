2c5195b313109fe3f4d97acaf08dbed24bb7b2da
Azham1999 <nmazham320@gmail.com>
2025-07-04 23:20:28 +0530

    Commit initial files

---
+++ index.js
@@ -0,0 +1,21 @@
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+
+let answerOne = document.getElementById("answer-one")
+let answerTwo = document.getElementById("answer-two")
+let button = document.getElementById("generator")
+
+function generatepassword() {
+  let result = "";
+  for (let i = 0; i < 15; i++) {
+    result += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return result;
+}
+    
+ 
+function gen() {
+    answerOne.textContent = generatepassword()
+    answerTwo.textContent = generatepassword()
+}
+
