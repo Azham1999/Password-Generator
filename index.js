6959ce9004193b3c254f6d3cdc6a7b9b5b552d2a
Azham1999 <nmazham320@gmail.com>
2025-07-21 03:10:37 +0530

    The final submit

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
+let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
 
 let answerOne = document.getElementById("answer-one")
 let answerTwo = document.getElementById("answer-two")
-let button = document.getElementById("generator")
 let answerIntro = document.getElementById("answer-intro")
+let userInput = document.getElementById("user-input")
 
 function generatepassword() {
-  let result = "";
-  for (let i = 0; i < 15; i++) {
+  let result = ""
+  if (userInput.value <= 18) {
+    answerIntro.innerText = "Here we go! Your strong passwords"
+    for (let i = 0; i < userInput.value; i++) {
     result += characters[Math.floor(Math.random() * characters.length)];
+    }
+  return result
+  } else {
+      answerIntro.innerText = "Sorry the password length is out of range!"
   }
-  return result;
 }
 
-let words = "Here we go! Your strong passwords"
 
 function gen() {
-    answerOne.textContent = generatepassword()
-    answerTwo.textContent = generatepassword()
-    answerIntro.textContent = words
+  answerOne.textContent = generatepassword()
+  answerTwo.textContent = generatepassword()
 }
-
